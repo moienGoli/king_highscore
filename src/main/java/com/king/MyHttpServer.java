c64ad8acd5fd11f3d313ba71701a1daf601c53f9
moien <sir.moien@gmail.com>
2017-09-12 15:35:52 +0430

    JustOptimistic

--- src/main/java/com/king/MyHttpServer.java
+++ src/main/java/com/king/MyHttpServer.java
@@ -45,7 +45,7 @@ public class MyHttpServer {
 
     private static class RequestHandler implements HttpHandler {
 
-        private static final int SESSION_KEY_TTL_SECONDS = 600;
+        private static final int SESSION_TTL_SECONDS = 600;
 
         public void handle(HttpExchange http) throws IOException {
 
@@ -81,7 +81,7 @@ public class MyHttpServer {
         private String handleLoginRequest(String uri) {
 
             int userId = helper.getUserIDFromLoginURI(uri);
-            return auth.doLogin(userId, SESSION_KEY_TTL_SECONDS);
+            return auth.doLogin(userId, SESSION_TTL_SECONDS);
         }
 
         private String handleHighScoreRequest(String uri) {
